dfd091e6abef446766ab956ca50e6ecf3dbb4667
Alexandre barbier <alexandr.barbier@gmail.com>
2016-06-16 12:11:01 +0200

    update test

--- CollabboardTests/CollabboardTests.swift
+++ CollabboardTests/CollabboardTests.swift
@@ -21,6 +21,7 @@ class CollabboardTests: XCTestCase {
     }
     
     func testExample() {
+        XCTAssert(true)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
